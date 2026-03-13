d97368312b3c33248d7e26667754372a25ed9a3c
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-13 10:00:48 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -2,7 +2,18 @@
 
 int main()
 {
+    int age;
+    char grade;
+    float gpa;
 
+    printf("Enter age: ") ;
+    scanf("%d", &age) ;
 
+    printf("Enter grade: ") ;
+    scanf("%c", &grade) ;
+
+    printf("Enter gpa: ") ;
+    scanf("%f", &gpa) ;
+    
     return 0;
 }
