8fc72ae76eb6baac9f0a78282d8c8bf8ad7b4a1f
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-19 03:54:22 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-     printf("%s %s %6s\n", "name", "        id", "value");
-    printf("%s %s %6.2f\n", " kim", "0000000012", 4.12);
-    printf("%s %s %6.2f\n", " lee", "0000001922", 3.21);
-    printf("%s %s %6.2f\n", "park", "0000000432", 1.0);
+     printf("%4s %10s %6s\n", "name", "        id", "value");
+    printf("%4s %10s %6.2f\n", " kim", "0000000012", 4.12);
+    printf("%4s %10s %6.2f\n", " lee", "0000001922", 3.21);
+    printf("%4s %10s %6.2f\n", "park", "0000000432", 1.0);
 
     return 0;
 }
