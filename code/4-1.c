936dbfdacd290bfbff446e28c918dcba74af61a8
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-19 01:11:10 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,29 @@
 
 int main()
 {
+    int a, b;
+    char op;
 
+    scanf("%d %d %c", &a, &b, &op);
+
+    switch (op) {
+        case '+':
+            printf("%d\n", a + b);
+            break;
+        case '-':
+            printf("%d\n", a - b);
+            break;
+        case '*':
+            printf(%d\n", a * b);
+            break;
+        case '/':
+            if(b == 0) {
+                printf("0으로 나눌 수 없습니다.");
+            } else {
+            printf("%d\n", a / b);
+            }
+            break;
+    }
     return 0;
 }
 
