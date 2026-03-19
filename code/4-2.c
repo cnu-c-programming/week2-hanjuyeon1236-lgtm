3dafb22e14f91ab23af984caea97618fdf19b109
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-19 19:57:44 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-      for (i = 2; i <= 9; i++) {
-        printf("--- %d단 ---\n", i);
-        for (j = 1; j <= 9; j++) {
-            printf("%d x %d = %d\n", i, j, i * j);
-        }
-        printf("\n"); 
-    }
-
-    return 0;
+     int i, j;
+         for(i = 2; i<10; i++){
+         printf("%d단 \n", i);
+         for(j = 1; j<10; j++){
+              printf("%d * %d = %d\n", i, j, i*j);
+         }
+         printf("\n");
+     }
+     return 0;
 }
