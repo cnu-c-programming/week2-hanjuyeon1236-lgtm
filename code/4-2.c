b69205b1e5a325241545af7352e6be906a69da1b
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-19 20:08:21 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -4,7 +4,6 @@ int main()
 {
      int i, j;
          for(i = 1; i<10; i++){
-         printf("%d\n", i);
          for(j = 1; j<10; j++){
               printf("%d * %d = %d\n", i, j, i*j);
          }
